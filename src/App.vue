b899a6cb63451c9ead94a29ea6a1bd01678469d0
Rúnar Berg <runar.BaugssonSigridarson@netapp.com>
2019-04-17 09:37:24 -0700

    Provide minimal example
    This example uses both `.flat()` in a template and `.flatMap()` as a
    computed property.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ol>
+      <li v-for="[i, j] of lists.flat()" :key="`${i}-${j}`">
+        {{ i + 1 }} - {{ String.fromCodePoint(j + 0x61) }}
+      </li>
+    </ol>
+
+    <ol>
+      <li v-for="item of flattened" :key="item" v-text="item" />
+    </ol>
   </div>
 </template>
 
@@ -12,6 +21,22 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+
+  data() {
+    return {
+      lists: Array.from({ length: 3 }, (_, i) =>
+        Array.from({ length: 3 }, (_, j) => [i, j])
+      )
+    };
+  },
+
+  computed: {
+    flattened() {
+      return this.lists.flatMap(list =>
+        list.map(([i, j]) => `${i + 1} - ${String.fromCodePoint(j + 0x61)}`)
+      );
+    }
   }
 }
 </script>
